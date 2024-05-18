54505d62bbd29af704e2f62d1c66b306cacd1d3c
blue-slushy9 <ra.cruz971@gmail.com>
2024-05-18 14:57:56 -0700

    moved Apple updates to the end of the script, that way i won't need to run it twice since many times the Mac will auto-reboot after the macOS updates

--- mac_brew_updates.sh
+++ mac_brew_updates.sh
@@ -1,44 +1,48 @@
 #!/bin/bash
 
 # This script will take care of nearly all updates for Mac, including
-# Apple updates and HomeBrew updates;
+# Apple and HomeBrew updates
 
-# Print a blank line to enhance legibility in Terminal;
+# Print a blank line to enhance legibility in Terminal
 echo
 
-# Install all available updates from Apple, if any;
-# & will run the command in the background;
-softwareupdate -i -a &
-
-# wait is used to wait for the background command above to finish running;
-wait
-
-# Print a blank line to enhance legibility in Terminal;
-echo
-
-# Enable verbose mode for brew;
+# Enable verbose mode for brew
 export HOMEBREW_VERBOSE=1
 
-# Update HomeBrew repos in background;
+# Update HomeBrew repos in background
 brew update &
 
-# wait for command to finish running;
+# wait for command to finish running
 wait
 
-# Blank line;
+# Blank line
 echo
 
-# Install all available formula updates in background;
+# Install all available formula updates in background
 brew upgrade &
 
-# Wait for background process to finish running;
+# Wait for background process to finish running
 wait
 
-# Blank line;
+# Blank line
 echo
 
-# Install all available cask updates;
+# Install all available cask updates
 brew upgrade --cask
 
-# Disable verbose mode;
+# Disable verbose mode
 unset HOMEBREW_VERBOSE
+
+# Blank line
+echo
+
+# Moved Apple updates to the bottom because they often require a reboot, this
+# way the script won't need to be run again after rebooting -- 5/18/24
+
+# Install all available updates from Apple, if any;
+# & will run the command in the background
+softwareupdate -i -a &
+
+# wait is used to wait for the background command above to finish running
+#wait # Dont't think this is necessary as softwareupdate reboots the Mac on
+# its own as needed
